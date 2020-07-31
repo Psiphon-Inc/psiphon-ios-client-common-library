32a7d237847566bbe7320075f6b956e8eb874844
Amir Khan <a.khan@psiphon.ca>
2020-07-31 13:56:51 -0400

    Update localized string "Change region" and "Fastest Country"
    Updated "Change region" to "Select server region"
    Updated "Fastest Country" to "Best performance"

--- PsiphonClientCommonLibrary/Classes/Settings/RegionAdapter.m
+++ PsiphonClientCommonLibrary/Classes/Settings/RegionAdapter.m
@@ -99,7 +99,7 @@
 
 + (NSDictionary*)getLocalizedRegionTitles {
 	return @{
-			 kPsiphonRegionBestPerformance: NSLocalizedStringWithDefaultValue(@"SERVER_REGION_FASTEST_COUNTRY", nil, [PsiphonClientCommonLibraryHelpers commonLibraryBundle], @"Fastest Country",@"The name of the pseudo-region a user can select if they want to use a Psiphon server with the best performance -- speed, latency, etc., rather than specify a particular region/country. This appears in a combo box and should be kept short."),
+			 kPsiphonRegionBestPerformance: NSLocalizedStringWithDefaultValue(@"SERVER_REGION_BEST_PERFORMANCE", nil, [PsiphonClientCommonLibraryHelpers commonLibraryBundle], @"Best performance",@"The name of the pseudo-region a user can select if they want to use a Psiphon server with the best performance -- speed, latency, etc., rather than specify a particular region/country. This appears in a combo box and should be kept short."),
 			 @"AT": NSLocalizedStringWithDefaultValue(@"SERVER_REGION_AT", nil, [PsiphonClientCommonLibraryHelpers commonLibraryBundle], @"Austria", @"Name of a country/region where Psiphon servers are located. The user can choose to only use servers in that country."),
              @"AU": NSLocalizedStringWithDefaultValue(@"SERVER_REGION_AU", nil, [PsiphonClientCommonLibraryHelpers commonLibraryBundle], @"Australia", @"Name of a country/region where Psiphon servers are located. The user can choose to only use servers in that country."),
 			 @"BE": NSLocalizedStringWithDefaultValue(@"SERVER_REGION_BE", nil, [PsiphonClientCommonLibraryHelpers commonLibraryBundle], @"Belgium", @"Name of a country/region where Psiphon servers are located. The user can choose to only use servers in that country."),
